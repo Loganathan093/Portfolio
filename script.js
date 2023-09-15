f66af2565b92ecc120596c85f0aa08c5b843f39e
M <loganathan.a.m@capgemini.com>
2023-09-15 10:45:22 +0530

    added projects

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 const lg = document.getElementById('lg');
 const lk = document.getElementById('lk');
-const nme = document.getElementById('name');
-nme.addEventListener("mouseover", function(e)
-{
-    lg.textContent = "Loki"
-    lk.textContent = "Loganathan"
-})
+var nme = document.getElementById('name');
 
-nme.addEventListener("mouseleave", function(e)
-{
-    lk.textContent = "Loki"
-    lg.textContent = "Loganathan"
-})
+
 
 function projectOpen()
 {
     window.open("projects.html");
 }
 
+function p1()
+{
+    window.open("https://loganathan093.github.io/Guess-Number/");
+}
+
 function linkOpen()
 {
     window.open("https://in.linkedin.com/");
 }
+
+setTimeout(function(){
+    console.log(nme.textContent);
+}, 2000)
+
